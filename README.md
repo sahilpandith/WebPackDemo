a0f61674e54139686f18b5991273d0e8623a050c
“sahilpandith” <sahil.romit@gmail.com>
2023-05-14 21:30:37 +0530

    asset handling

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 npm init -y
 npm install webpack webpack-cli --save-dev
+
+//loading css
+npm install --save-dev style-loader css-loader
+
+
+loading data
+npm install --save-dev csv-loader xml-loader
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import _ from "lodash";
-import myName from "./myName"
+import myName from "./myName";
+import './style.css';
+import dogIcon from './dog.svg';
+import Data from './data.xml';
+import Notes from './data.csv';
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = myName('Cody');
+    element.innerHTML = myName('Hachiko');
+    element.classList.add('hello');
   
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = dogIcon;
+
+    element.appendChild(myIcon);
+  
+    console.log(Data);
+    console.log(Notes);
+
     return element;
   }
   
